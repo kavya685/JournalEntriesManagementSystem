648c8589aeb1b985a78fb733540fcd324951ae43
kavya685 <kavyapopat95@gmail.com>
2026-05-11 19:44:41 +0530

    add weatherResponse.java for POJO and WeatherService.java for service logic

--- src/main/java/com/Journal/journalDemo/response/WeatherResponse.java
+++ src/main/java/com/Journal/journalDemo/response/WeatherResponse.java
@@ -1,13 +1,25 @@
 package com.Journal.journalDemo.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.ArrayList;
 
+@Data
 public class WeatherResponse {
+    @Getter
+    @Setter
     public Current current;
 
+    @Data
     public class Current{
+        @Getter
+        @Setter
         public int temperature;
-        public ArrayList<String> weather_descriptions;
+        //this below annotaion is for ensuring best practices(camelcase and snakecase)
+        @JsonProperty("weather_descriptions")
+        public ArrayList<String> weatherDescriptions;
         public int feelslike;
     }
 }
--- src/main/java/com/Journal/journalDemo/service/WeatherService.java
+++ src/main/java/com/Journal/journalDemo/service/WeatherService.java
@@ -1,5 +1,33 @@
 package com.Journal.journalDemo.service;
 
+import com.Journal.journalDemo.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class WeatherService {
-    public final String key = "ae2e15d03b62f26ddb6cb4ad9f165e81";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    // by using @Value, we make our key secure and not hard coded in our code,
+    // we can change it in application.properties file without changing the code
+    // make sure not to use static for the key variable, otherwise it will not work with @Value annotation
+    // this annotation works with instance variable, so using static is not recommended here
+    @Value("${weather.api.key}")
+    public String key;
+
+    public final String url = "https://api.weatherstack.com/current?access_key=what&query=New%20York";
+
+
+    public WeatherResponse getWeather()
+    {
+        String API = url.replace("what",key);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(API, HttpMethod.GET, null, WeatherResponse.class);
+        WeatherResponse body = response.getBody();
+        return body;
+    }
 }
